88badbafd02e07c0dbdb4c0e9e6e4f7ff0f52c19
Imago31 <xaker201301@gmail.com>
2024-11-22 05:32:36 +0100

    app developing

--- src/App.js
+++ src/App.js
@@ -16,41 +16,56 @@ function App() {
 
   const [vido] = useState(videoData);
   const [type, setType] = useState("ger");
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [mobielMenuButton, setMobielMenuButton] = useState(<span>&#9776;</span>);
 
   const handleclick = (e) =>{
-    console.log(e.target.id)
-    setType(e.target.id)
+    setType(e.target.id);
+    document.querySelector(".mobileMenu").style = "height: 70px";
+    setMobielMenuButton(<span>&#9776;</span>);
+    setMobileMenu(false);
+  }
+
+  const mobileMenuHandle = () =>{
+    setMobileMenu(!mobileMenu);
+    if(mobileMenu){
+      setMobielMenuButton(<span>&#9776;</span>);
+      document.querySelector(".mobileMenu").style = "height: 70px";
+    }
+    else{
+      setMobielMenuButton(<span>&#10005;</span>);
+      document.querySelector(".mobileMenu").style = "height: 100%";
+    }
   }
 
   return (
     <div className="main">
       <div className="menuSection">
         <div className="desktopMenu">
-                  <h1 className="title">YouTuDrive</h1>
-                  <div className="menu">
-                      <button id='ger' onClick={handleclick} className="menuButton" style={type=="ger" ? {color:"red",border: "2px solid red"} : {color:"orange",border: "2px solid orange"}}>German</button>
-                      <button id='en' onClick={handleclick} className="menuButton" style={type=="en" ? {color:"red",border: "2px solid red"} : {color:"orange",border: "2px solid orange"}}>English</button>
-                      <button id='ru' onClick={handleclick} className="menuButton" style={type=="ru" ? {color:"red",border: "2px solid red"} : {color:"orange",border: "2px solid orange"}}>Russian</button>
-                      <button id='car' onClick={handleclick} className="menuButton" style={type=="car" ? {color:"red",border: "2px solid red"} : {color:"orange",border: "2px solid orange"}}>Cars</button>
-                  </div>
+          <h1 className="title">YouTuDrive</h1>
+            <div className="menu">
+              <button id='ger' onClick={handleclick} className="menuButton" style={type=="ger" ? {color:"red",border: "2px solid red"} : {color:"orange",border: "2px solid orange"}}>German</button>
+              <button id='en' onClick={handleclick} className="menuButton" style={type=="en" ? {color:"red",border: "2px solid red"} : {color:"orange",border: "2px solid orange"}}>English</button>
+              <button id='ru' onClick={handleclick} className="menuButton" style={type=="ru" ? {color:"red",border: "2px solid red"} : {color:"orange",border: "2px solid orange"}}>Russian</button>
+              <button id='car' onClick={handleclick} className="menuButton" style={type=="car" ? {color:"red",border: "2px solid red"} : {color:"orange",border: "2px solid orange"}}>Cars</button>
+            </div>
         </div>
         <div className="mobileMenu">
-                <span className="openMenu closed">&#9776;</span>
+                <p className="openMenu" onClick={mobileMenuHandle}>{mobielMenuButton}</p>
                 <h1 className="title">YouTuDrive</h1>
                 <div className="menu">
-                      <button id='ger' onClick={handleclick} className="menuButton" style={type=="ger" ? {color:"red",border: "2px solid red"} : {color:"orange",border: "2px solid orange"}}>German</button>
-                      <button id='en' onClick={handleclick} className="menuButton" style={type=="en" ? {color:"red",border: "2px solid red"} : {color:"orange",border: "2px solid orange"}}>English</button>
-                      <button id='ru' onClick={handleclick} className="menuButton" style={type=="ru" ? {color:"red",border: "2px solid red"} : {color:"orange",border: "2px solid orange"}}>Russian</button>
-                      <button id='car' onClick={handleclick} className="menuButton" style={type=="car" ? {color:"red",border: "2px solid red"} : {color:"orange",border: "2px solid orange"}}>Cars</button>
+                  <button id='ger' onClick={handleclick} className="menuButton" style={type=="ger" ? {color:"red",border: "2px solid red"} : {color:"orange",border: "2px solid orange"}}>German</button>
+                  <button id='en' onClick={handleclick} className="menuButton" style={type=="en" ? {color:"red",border: "2px solid red"} : {color:"orange",border: "2px solid orange"}}>English</button>
+                  <button id='ru' onClick={handleclick} className="menuButton" style={type=="ru" ? {color:"red",border: "2px solid red"} : {color:"orange",border: "2px solid orange"}}>Russian</button>
+                  <button id='car' onClick={handleclick} className="menuButton" style={type=="car" ? {color:"red",border: "2px solid red"} : {color:"orange",border: "2px solid orange"}}>Cars</button>
                 </div>
             </div>
         <div className="videoSection">
           {
           videoData.map((item, index) =>(
-           item.type == type && <iframe key={index} className="yutu"  src={item.video} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+           item.type == type && <iframe key={index} src={item.video} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
           ))
           }
-          
         </div>
       </div>
     </div>
